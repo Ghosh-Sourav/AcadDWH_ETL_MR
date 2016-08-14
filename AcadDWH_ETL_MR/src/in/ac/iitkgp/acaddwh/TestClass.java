7a2f709f00eba8e549f47ab1cddce603675a2f08
Sourav Ghosh <sourav.ghosh.840001@gmail.com>
2016-08-14 22:05:17 +0530

    Web server info changed; test code fixed

--- AcadDWH_ETL_MR/src/in/ac/iitkgp/acaddwh/TestClass.java
+++ AcadDWH_ETL_MR/src/in/ac/iitkgp/acaddwh/TestClass.java
@@ -6,31 +6,29 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
-
 public class TestClass {
 
 	public static void moveFile() {
-		String localfsFilePath = "/home/sourav/AcadDWH/test_table_data.csv";
-		String hdfsFilePath = "/user/15CS60R16/AcadDWH/test_table_data.csv";
+		String localfsFilePath = "/home/mtech/15CS60R16/x.csv";
+		String hdfsFilePath = "/user/15CS60R16/TestDir/x.csv";
 
 		try {
 			Configuration config = new Configuration();
-			config.set("fs.defaultFS", "hdfs://10.5.30.101:18020");
+			config.set("fs.defaultFS", "hdfs://cse-hadoop-109:8020");
 			FileSystem hdfs = FileSystem.get(config);
 			Path srcPath = new Path(localfsFilePath);
 			Path dstPath = new Path(hdfsFilePath);
 			hdfs.copyFromLocalFile(srcPath, dstPath);
+			System.out.println("Initiated move...");
+
 		} catch (IOException e) {
-			System.out.println("Irrelevant IOException occurred in TestDAO... ignored! "+e.getMessage());
-		}
-		finally {
-			System.out.println("Initiated move... Waiting...");
-			
-			System.out.println("File move assumed to be complete!");
-			
+			e.printStackTrace();
+
+		} finally {
+			System.out.println("File move processed!");
+
 		}
 
 	}
 
-	
 }
