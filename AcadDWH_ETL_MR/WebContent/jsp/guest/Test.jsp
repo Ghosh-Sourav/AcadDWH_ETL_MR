60fef0fe1b5b6edbc85d6ba2d8e618bdcd23c4de
Ghosh-Sourav <sourav.9@tcs.com>
2016-08-16 03:30:02 +0530

    HDFSList Get files test code added

--- AcadDWH_ETL_MR/WebContent/jsp/guest/Test.jsp
+++ AcadDWH_ETL_MR/WebContent/jsp/guest/Test.jsp
@@ -1,4 +1,4 @@
-<%@page import="in.ac.iitkgp.acaddwh.TestMR"%>
+<%@page import="in.ac.iitkgp.acaddwh.TestHDFSList"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -10,7 +10,12 @@
 <body>
 Calling TestClass...
 <%
-out.println(TestMR.executeJob());
+//out.println(TestMR.executeJob());
+
+for (String filePath: TestHDFSList.getFiles()) {
+	out.println(filePath);
+}
+
 %>
 <br/>
 TestClass called!
---
+++ AcadDWH_ETL_MR/src/in/ac/iitkgp/acaddwh/TestHDFSList.java
@@ -0,0 +1,32 @@
+package in.ac.iitkgp.acaddwh;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import in.ac.iitkgp.acaddwh.config.HadoopNodeInfo;
+import in.ac.iitkgp.acaddwh.config.NameNodeInfo;
+
+public class TestHDFSList {
+	public static List<String> getFilePaths() throws Exception {
+		List<String> filePaths = new ArrayList<String>();
+		try {
+			FileSystem fs = FileSystem.get(new Configuration());
+			FileStatus[] fileStatus = fs.listStatus(new Path(NameNodeInfo.getUrl() + HadoopNodeInfo.getPathInHdfs()
+					+ "outputDir_20160815213022114_INSA_dim_departments_csv"));
+
+			for (FileStatus eachFileStatus : fileStatus) {
+				if (eachFileStatus.isFile() && eachFileStatus.getPath().getName().contains("part-r-")) {
+					filePaths.add(eachFileStatus.getPath().toString());
+		        }
+			}
+		} catch (Exception e) {
+			System.out.println("File not found");
+		}
+		return filePaths;
+	}
+}
