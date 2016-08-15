e2e3c30b43c0733956dedf5e19f89a737637f54e
Sourav Ghosh <sourav.ghosh.840001@gmail.com>
2016-08-16 03:04:48 +0530

    Consolidated test code till "processing dept instance file with MR"

--- AcadDWH_ETL_MR/src/in/ac/iitkgp/acaddwh/TestMR.java
+++ AcadDWH_ETL_MR/src/in/ac/iitkgp/acaddwh/TestMR.java
@@ -13,6 +13,7 @@ import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import in.ac.iitkgp.acaddwh.config.HadoopNodeInfo;
+import in.ac.iitkgp.acaddwh.config.NameNodeInfo;
 
 public class TestMR {
 	public static class ETMapper extends Mapper<Text, Text, Text, Text> {
@@ -20,40 +21,43 @@ public class TestMR {
 
 		public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
 			StringTokenizer itr = new StringTokenizer(value.toString(), ",");
-			
-			StringBuffer sb = new StringBuffer(); 
+
+			StringBuffer sb = new StringBuffer();
 			while (itr.hasMoreTokens()) {
-				sb.append("_<"+itr.nextToken()+">_");
+				sb.append("_<" + itr.nextToken() + ">_");
+				sb.append(",");
 			}
+			sb.setLength(sb.length()-1);
+			
 			attribute.set(sb.toString());
 			context.write(key, attribute);
 		}
 	}
-/*
-	public static class ETReducer extends Reducer<Text, Text, Text, Text> {
-		private Text result = new Text();
 
-		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-			String translations = "";
-			for (Text val : values) {
-				translations += "|" + val.toString();
-			}
-			result.set(translations);
-			context.write(key, result);
-		}
-	}
-*/
+	/*
+	 * public static class ETReducer extends Reducer<Text, Text, Text, Text> {
+	 * private Text result = new Text();
+	 * 
+	 * public void reduce(Text key, Iterable<Text> values, Context context)
+	 * throws IOException, InterruptedException { String translations = ""; for
+	 * (Text val : values) { translations += "|" + val.toString(); }
+	 * result.set(translations); context.write(key, result); } }
+	 */
 	public static int executeJob() throws Exception {
 		Configuration conf = new Configuration();
+		conf.set("key.value.separator.in.input.line", ",");
+		conf.set("mapred.textoutputformat.separator", ",");
 		Job job = new Job(conf, "testMR");
 		job.setJarByClass(TestMR.class);
 		job.setMapperClass(ETMapper.class);
-		//job.setReducerClass(ETReducer.class);
+		// job.setReducerClass(ETReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
 		job.setInputFormatClass(KeyValueTextInputFormat.class);
-		FileInputFormat.addInputPath(job, new Path(HadoopNodeInfo.getPathInHdfs() + "20160815213022114_INSA_dim_departments.csv"));
-		FileOutputFormat.setOutputPath(job, new Path(HadoopNodeInfo.getPathInHdfs() + "output_20160815213022114_INSA_dim_departments.csv"));
+		FileInputFormat.addInputPath(job, new Path(
+				NameNodeInfo.getUrl() + HadoopNodeInfo.getPathInHdfs() + "20160815213022114_INSA_dim_departments.csv"));
+		FileOutputFormat.setOutputPath(job, new Path(NameNodeInfo.getUrl() + HadoopNodeInfo.getPathInHdfs()
+				+ "outputDir_20160815213022114_INSA_dim_departments_csv"));
 		return (job.waitForCompletion(true) ? 0 : 1);
 	}
 }
